65e8baae74b75df1adb92a9fc88efb2d4e8cc3f2
enes1094 <enes.tikici123@hotmail.com>
2023-07-10 22:37:01 +0300

    added Configuration

--- AspNetCoreMvc2.Introduction/Startup.cs
+++ AspNetCoreMvc2.Introduction/Startup.cs
@@ -2,26 +2,73 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreMvc2.Introduction.Entities;
+using AspNetCoreMvc2.Introduction.Identity;
 using AspNetCoreMvc2.Introduction.Model;
 using AspNetCoreMvc2.Introduction.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreMvc2.Introduction
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            var connection = @"Server=(localdb)\mssqllocaldb;Database=SchoolDb; Trusted_Connection=true";
-            services.AddDbContext<SchoolContext>(options => options.UseSqlServer(connection));
+            services.AddDbContext<SchoolContext>(options => options.UseSqlServer(_configuration["dbConnection"]));
+            services.AddIdentity<AppIdentityUser, AppIdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.RequireUniqueEmail = true;
+
+                options.SignIn.RequireConfirmedEmail = true;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+            });
+
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Security/Login";
+                options.LogoutPath = "/Security/Logout";
+                options.AccessDeniedPath = "/Security/AccesDenied";
+                options.SlidingExpiration = true;
+                options.Cookie = new CookieBuilder
+                {
+                    HttpOnly = true,
+                    Name = ".AspNetCoreDemo.Security.Cookie",
+                    Path = "/",
+                    SameSite = SameSiteMode.Lax,
+                    SecurePolicy = CookieSecurePolicy.SameAsRequest
+                };
+            });
+
             services.AddScoped<ICalculator, Calculator8>();
             services.AddSession();
             services.AddDistributedMemoryCache();
@@ -42,14 +89,20 @@ namespace AspNetCoreMvc2.Introduction
             }
             app.UseSession();
 
+            app.UseAuthentication();
+
             app.UseMvc(ConfigureRoutes);
-            
+
         }
 
         private void ConfigureRoutes(IRouteBuilder routeBuilder)
         {
             routeBuilder.MapRoute("Default", "{controller=Filter}/{action=Index}/{id?}");
             routeBuilder.MapRoute("MyRoute", "Enes/{controller=Home}/{action=Index3}/{id?}");
+            routeBuilder.MapRoute(
+              name: "areas",
+              template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+            );
         }
     }
 }
---
+++ AspNetCoreWithAngular/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWithAngular.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        public List<Customer> Get()
+        {
+            return new List<Customer>
+            {
+            new Customer { Id = 1, FirstName = "Enes", LastName = "Tikici" },
+            new Customer { Id = 1, FirstName = "Hamza", LastName = "Tikici" },
+            new Customer { Id = 1, FirstName = "Zehra", LastName = "Tikici" },
+            };
+        }
+
+    }
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
